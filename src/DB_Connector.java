abdcf213cf85d12f9907e336cd5897414bdf9ab0
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-10-12 08:45:23 +0200

    Initial commit

---
+++ src/DB_Connector.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_Connector {
+    // Declare a connection
+    private static Connection con = null;
+    // JDBC driver
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    // the url = jdbc://host name:port#/db name
+    private static String url = "jdbc:mysql://localhost:3306/firstdatabase";
+    // user name
+    private static String usr = "root";
+    // password
+    private static String password = ""; // Needs password
+
+    public static Connection connect(){
+        System.out.println("--Connecting to MySQL JDBC");
+        // Locate MySQL JDBC Driver
+        try {
+            Class.forName(DRIVER);
+        } // Catch exceptions if JDBC is not found
+        catch(ClassNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("--JDBC driver is missing--");
+
+        }
+        System.out.println("--MySQL JDBC driver registered--");
+
+        try { // Connect to MySQL DB = URL, userName, password
+            con = DriverManager.getConnection(url, usr, password);
+        } // Catch exceptions on connection error
+        catch (SQLException ex){
+            ex.printStackTrace();
+            System.out.println("--Did not connect try again--");
+        }
+        // if connection successful
+        if(con != null) {
+            System.out.println("--Connection successful--");
+        } else {
+            System.out.println("--Failed to connect--");
+        }
+        return con;
+    }
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args) {
+        DB_Connector db_connector = new DB_Connector();
+        db_connector.connect();
+    }
+
+}
