bfe03b946041fceaca0ecc8aa11af0e5ba6330f7
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-10-27 10:18:23 +0200

    Updates.

--- src/DB_Statements.java
+++ src/DB_Statements.java
@@ -11,16 +11,16 @@ public class DB_Statements {
     private static Connection con = DB_Connector.connect();
 
     // Method to create a new Database
-    public void createNewDB() {
+    public void createNewDB(String DB_Name) {
         // SQL Statement
-        String query = "create database if not exists ThisDatabase";
+        String query = "create database if not exists " + DB_Name;
 
         try {
             // Connection
             stmt = con.createStatement();
             // Execute statement
             stmt.executeUpdate(query);
-            System.out.println("\n--Database created--");
+            System.out.println("\n--Database " + DB_Name + " created--");
         }
         catch (SQLException ex) {
             // Handle SQL exceptions
@@ -28,4 +28,42 @@ public class DB_Statements {
             ex.printStackTrace();
         }
     }
+
+    // Method to use a database
+    public void useDB(String DB_Name) {
+        // Statement
+        String query = "use " + DB_Name;
+        try {
+            // Connection
+            stmt = con.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("\n--Using " + DB_Name + "--");
+        } catch (SQLException ex) {
+            // Handle SQL exceptions
+            System.out.println("\n--Query did not execute--");
+            ex.printStackTrace();
+        }
+    }
+
+        public void createTable(String tableName) {
+            //SQL statement
+        String query = "create table if not exists " + tableName + "(" +
+                "id int not null auto_increment, " +
+                "myName varchar(28), " +
+                "address varchar(28), " +
+                "primary key (id)" +
+                ")";
+        try {
+            // Connection
+            stmt = con.createStatement();
+            // Execute query
+            stmt.executeUpdate(query);
+            System.out.println("\n--Table " + tableName + " created--");
+        }   catch (SQLException ex) {
+            System.out.println("\n--Query did not execute--");
+            ex.printStackTrace();
+        }
+    }
+
+
 }
--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,9 @@ public class Test {
     public static void main(String[] args) {
         // DB_Connector.connect();
         DB_Statements stmts = new DB_Statements();
-        stmts.createNewDB();
+        //stmts.createNewDB();
+        stmts.useDB("ThisDatabase");
+        stmts.createTable("MyTable");
     }
 
 
